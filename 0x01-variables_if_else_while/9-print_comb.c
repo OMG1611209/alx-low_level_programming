35db3156d81f5bb62d56efe2e9f275d6fbd80061
OMG1611209 <Omar209Khalaf@gmail.com>
2023-09-09 02:57:53 +0300

    hi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,9 +15,9 @@ int main(void)
 
 		if (i != '9')
 		{
-																		putchar(',');
-																		putchar(' ');
-																	}
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
