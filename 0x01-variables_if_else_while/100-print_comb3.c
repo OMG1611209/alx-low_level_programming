73345c6669d57f6738a6038b9893d97add7926ab
OMG1611209 <Omar209Khalaf@gmail.com>
2023-09-09 03:24:25 +0300

    hi

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-		int i = 0;
-		int j;
-		int count = 0;
+	int i = 0;
+	int j;
+	int count = 0;
 
-		while (i <= 8)
-							{
+	while (i <= 8)
+	{
 		j = i + 1;
 		while (j <= 9)
 		{
 			putchar(i + '0');
-																								putchar(j + '0');
+																								putchar(j + '0');		
 																								if (count != 44)
 																								{
 																									putchar(',');
