a41d0e73960711e6a5767c522c36d578f04f0b35
OMG1611209 <Omar209Khalaf@gmail.com>
2023-11-15 22:53:16 +0200

    hi

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,10 +17,10 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (product < 10)
-				{
-					_putchar(' ');
-				}
+			if (product < 10)
+			{
+				_putchar(' ');
+			}
 			}			
 			if (product >= 10)
 			{
