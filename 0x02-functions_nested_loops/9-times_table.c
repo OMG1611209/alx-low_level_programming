20a9fb83d0dff08eba0bc11ce85b3151fa6b0b1a
OMG1611209 <Omar209Khalaf@gmail.com>
2023-11-15 22:59:14 +0200

    hi

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,11 +7,9 @@
  */
 void times_table(void)
 {
-	int i, j;
-
-	for (i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (int j = 0; j < 10; j++)
 		{
 			int product = i * j;
 
